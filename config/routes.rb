d9a599bfdd5aec6e00e984b864f5c13451661b08
Sara Schoonover <Saramschoon@gmail.com>
2023-10-09 21:56:02 +0000

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
+  # Routes for the Movie resource:
+
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  patch("/movies/:id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "movies#index"
 end
